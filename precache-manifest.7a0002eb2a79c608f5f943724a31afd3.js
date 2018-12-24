e9244c21a452ff13b05bf05bb36c5481e2d5a475
YashishDua <yashish.dua@gmail.com>
2018-12-25 01:33:16 +0530

    Updates

---
+++ precache-manifest.7a0002eb2a79c608f5f943724a31afd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d901b838b8d99373fd4af633b3ef29eb",
+    "url": "/yashishdua.github.io/static/media/Yashish.d901b838.jpg"
+  },
+  {
+    "revision": "fe5d76cb8a37e75ee4ca",
+    "url": "/yashishdua.github.io/static/js/runtime~main.fe5d76cb.js"
+  },
+  {
+    "revision": "b95a3c4973237f3963b1",
+    "url": "/yashishdua.github.io/static/js/main.b95a3c49.chunk.js"
+  },
+  {
+    "revision": "cf44e5e94998ec15b282",
+    "url": "/yashishdua.github.io/static/js/1.cf44e5e9.chunk.js"
+  },
+  {
+    "revision": "b95a3c4973237f3963b1",
+    "url": "/yashishdua.github.io/static/css/main.38a8b60b.chunk.css"
+  },
+  {
+    "revision": "d81352b10e6f55c79646f6603fcdd34c",
+    "url": "/yashishdua.github.io/index.html"
+  }
+];
